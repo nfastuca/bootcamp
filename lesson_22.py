32caa74ee8bb204706f8c99545b3e3dc5fbc15b6
Nick Fastuca <nfastuca@caltech.edu>
2016-09-13 14:18:08 -0700

    lesson 22 plotted as data points, not line

--- lesson_22.py
+++ lesson_22.py
@@ -9,7 +9,10 @@ x = np.linspace(-15, 15, 400)
 #compute the normalized intensity
 norm_I = 4 * (scipy.special.j1(x)/x)**2
 
-#Plot our computation
-plt.plot(x, norm_I)
+#Plot our computed data points with markers, no line
+plt.plot(x, norm_I, marker='.', linestyle='none')
+
+#expand margins by 2%
+plt.margins(0.02)
 plt.xlabel('$x$')
 plt.ylabel('$I(x) / I_0$')
