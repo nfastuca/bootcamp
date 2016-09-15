30893647e9a70e86b2aba1cbe7b16d3957a8ee8d
Nick Fastuca <nfastuca@caltech.edu>
2016-09-14 21:07:32 -0700

    sliced the sperm data

--- exercise_4_2.py
+++ exercise_4_2.py
@@ -8,16 +8,20 @@ sns.set()
 bee_weight = pd.read_csv('data/bee_weight.csv', comment='#')
 bee_sperm = pd.read_csv('data/bee_sperm.csv', comment='#')
 
+#Munch data, create df of weight and sperm quality for Pestice and control bees
 p_weights = bee_weight.loc[(bee_weight['Treatment'] == 'Pesticide'), 'Weight']
 c_weights = bee_weight.loc[(bee_weight['Treatment'] == 'Control'), 'Weight']
+p_sperm = bee_sperm.loc[(bee_sperm['Treatment'] == 'Pesticide'), 'Quality']
+c_sperm = bee_sperm.loc[(bee_sperm['Treatment'] == 'Control'), 'Quality']
+
 x_pw, y_pw = hs.ecdf(p_weights)
 x_cw, y_cw = hs.ecdf(c_weights)
 
-plt.plot(x_pw, y_pw, marker='.', linestyle='none', markersize=10, alpha=0.5)
-plt.plot(x_cw, y_cw, marker='.', linestyle='none', markersize=10, alpha=0.5)
-plt.xlabel('Bee Weight', size=20)
-plt.ylabel('ECDF', size=20)
-plt.legend(('Pesticide', 'Control'), loc='lower right', fontsize=12)
+# plt.plot(x_pw, y_pw, marker='.', linestyle='none', markersize=10, alpha=0.5)
+# plt.plot(x_cw, y_cw, marker='.', linestyle='none', markersize=10, alpha=0.5)
+# plt.xlabel('Bee Weight', size=20)
+# plt.ylabel('ECDF', size=20)
+# plt.legend(('Pesticide', 'Control'), loc='lower right', fontsize=12)
 
 #compute mean weights
 p_mean = np.mean(p_weights)
